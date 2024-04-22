f1956df1e56eb6a78c8c5550ba13f38604fd98e4
Alp Eren Sağdıç <alperensagdicfb@gmail.com>
2024-04-22 17:31:22 +0300

    updated Station class
    Added getters and setters to Station class.

--- Station.java
+++ Station.java
@@ -10,4 +10,91 @@ public class Station {
     private boolean MULTIFLAG;
     private boolean FIFOFLAG;
     private boolean idle;
+
+    public Station(String stationID, int maxCapacity, double stationSpeed, double stationSpeedPercentage, boolean MULTIFLAG, boolean FIFOFLAG) {
+        this.stationID = stationID;
+        this.maxCapacity = maxCapacity;
+        this.stationSpeed = stationSpeed;
+        this.stationSpeedPercentage = stationSpeedPercentage;
+        this.MULTIFLAG = MULTIFLAG;
+        this.FIFOFLAG = FIFOFLAG;
+        this.idle = true;
+        this.waitingTasks = new ArrayList<>();
+        this.handleTypes = new ArrayList<>();
+    }
+
+    public String getStationID() {
+        return stationID;
+    }
+
+    public void setStationID(String stationID) {
+        this.stationID = stationID;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getStationSpeed() {
+        return stationSpeed;
+    }
+
+    public void setStationSpeed(double stationSpeed){
+        this.stationSpeed = stationSpeed;
+    }
+
+    public double getStationSpeedPercentage() {
+        return stationSpeedPercentage;
+    }
+
+    public void setStationSpeedPercentage(double stationSpeedPercentage) {
+        this.stationSpeedPercentage = stationSpeedPercentage;
+    }
+
+    public ArrayList<Task> getWaitingTasks() {
+        return waitingTasks;
+    }
+
+    public void setWaitingTasks(ArrayList<Task> waitingTasks) {
+        this.waitingTasks = waitingTasks;
+    }
+
+    public ArrayList<String> getHandleTypes() {
+        return handleTypes;
+    }
+
+    public void setHandleTypes(ArrayList<String> handleTypes) {
+        this.handleTypes = handleTypes;
+    }
+
+    public boolean isMULTIFAG() {
+        return MULTIFLAG;
+    }
+
+    public void setMULTIFAG(boolean MULTIFLAG) {
+        this.MULTIFLAG = MULTIFLAG;
+    }
+
+    public boolean isFIFOFLAG() {
+        return FIFOFLAG;
+    }
+
+    public void setFIFOFLAG(boolean FIFOFLAG) {
+        this.FIFOFLAG = FIFOFLAG;
+    }
+
+    public boolean isIdle() {
+        return idle;
+    }
+
+    public void setIdle(boolean idle) {
+        this.idle = idle;
+    }
+
+    // TODO: Adding and Removing Tasks
+
 }
