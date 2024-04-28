ff85467f4ab91bf3cce17bb29559d3be216386c2
Emin Arslan <arslanemin2662@gmail.com>
2024-04-28 17:31:29 +0300

    Added a function to read the workflow file, added InvalidSyntaxException, updated main to read from a test workflow file and print the created TaskType objects

--- Main.java
+++ Main.java
@@ -1,21 +1,110 @@
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.lang.Integer;
 public class Main {
+	public static class InvalidSyntaxException extends Exception {
+		public InvalidSyntaxException(String message, String fname, int lineNumber) {
+			super("[line #" + lineNumber + " in file '" + fname + "'] " + message);
+		}
+	}
+	private static ArrayList<TaskType> taskTypes;
+	private static int lineNumber = 1;
+
+	private static void skipWhiteSpace(Scanner sc) {
+		while (sc.hasNext("\\s")) {
+			if (sc.hasNext("\n")) {
+				lineNumber++;
+			}
+			sc.skip("\\s");
+		}
+	}
+	private static void readTaskTypes(Scanner sc, String fname) throws InvalidSyntaxException {
+		skipWhiteSpace(sc);
+		taskTypes = new ArrayList<>();
+		
+		// check for beginning parenthesis
+		sc.useDelimiter("[^(]");
+		if (!sc.hasNext("[(]")) {
+			throw new InvalidSyntaxException("line doesn't start with '('", fname, lineNumber); 
+		}
+		sc.next("[(]");
+		sc.useDelimiter("\\s+");
+
+		// check for the TASKTYPES word
+		String word = sc.next("\\w*");
+		if (!word.equals("TASKTYPES")) 
+			throw new InvalidSyntaxException("expected \"TASKTYPES\", found \"" + word + "\"", fname, lineNumber);
+		
+		// parse each task type and add it to the global list of tasktypes.
+		// TODO: maybe add the global list of task types as a static member of TaskType
+		// instead. It doesn't really fit in main.
+		for(;;) {
+			skipWhiteSpace(sc);
+			sc.useDelimiter("[^\\w.]+");
+			if (!sc.hasNext("[A-Za-z][A-Za-z_0-9]*")) {
+				// next word isn't available. check if we're at the end of the
+				// list, otherwise signal exception
+				sc.useDelimiter("[^)]");
+				boolean has = sc.hasNext("[)]");
+				sc.useDelimiter("\\s+");
+				if (!has) {
+					throw new InvalidSyntaxException("expected ')' "  + sc.nextLine(), fname, lineNumber);
+				}
+				break;
+			}
+			String typeName = sc.next("\\w*");
+			
+			skipWhiteSpace(sc);
+			// check for default size
+			String defaultSize = "";
+			if (sc.hasNext("-?[0-9]+([.][0-9]+)?")) {
+				System.out.println();
+				defaultSize = sc.next("-?[0-9]+(\\.[0-9]+)?");
+
+				// TODO: change this to use the double directly
+				// once TaskType is updated to use double as well.
+				int parsed = (int)Double.parseDouble(defaultSize);
+				if (parsed < 0) {
+					throw new InvalidSyntaxException("TaskType " + typeName + " has negative default size.", fname, lineNumber);
+				}
+				
+				taskTypes.add(new TaskType(typeName, parsed));
+			} else {
+				taskTypes.add(new TaskType(typeName));
+			}
+		}
+	}
+
+	public static void readWorkFlowFile(String fname) throws InvalidSyntaxException {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(Paths.get(fname));
+		} catch (Exception e) {
+			System.out.println("Error: Failed to open workflow file '" + fname + "'");
+			return;
+		}
+		
+		// only task types for now, later on JobTypes and Stations will be read here
+		// as well.
+		readTaskTypes(sc, fname);
+	}
+
 	public static void readJobFile(String fname) {
 		/* Try and open the file. Print an error message on failure. */
 		Scanner sc = null;
 		try {
 			sc = new Scanner(Paths.get(fname));
 		} catch (Exception e) {
-			System.out.println("Error: Failed to open file '" + fname + "'");
+			System.out.println("Error: Failed to open job file '" + fname + "'");
 			return;
 		}
+
 		/* Now we have a real scanner, we can parse the file. 
 		 * The file format has no multi-line structures, so we will
 		 * parse line-by-line.
 		 */
-		int lineNumber = 0; // keep track of line number for error messages
+		int lineNumber = 1; // keep track of line number for error messages
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 
@@ -52,7 +141,11 @@ public class Main {
 			lineNumber++;
 		}
 	}
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception{
+		//TODO: catch exceptions
+		readWorkFlowFile("workflow.txt");
+		for (TaskType i : taskTypes) {
+			System.out.printf("TaskType{ name: %s, defaultSize: %d}%n", i.getTaskTypeName(), i.getDefaultTaskSize());
+		}
 	}
 }
