e7e3e66193b4ab51be72575236eb7495da576e36
hgorkemy <153822528+hgorkemy@users.noreply.github.com>
2024-04-30 16:55:28 +0300

    Update Job.java
    Constructor and Getters/Setters for Job Class

--- Job.java
+++ Job.java
@@ -1,2 +1,51 @@
 public class Job{
+  String jobID;
+  String jobTypeID;
+  int startTime;
+  int duration;
+  public Job(String jobID,String jobTyprID,int startTime,int duration){
+       this.jobID = jobID;
+       this.jobTypeID = jobTypeID;
+       this.startTime = startTime;
+       this.duration = duration;
+  }
+   public String getJobID() {
+        return jobID;
+    }
+
+    public void setJobID(String jobID) {
+        this.jobID = jobID;
+    }
+
+    public String getJobTypeID() {
+        return jobTypeID;
+    }
+
+    public void setJobTypeID(String jobTypeID) {
+        this.jobTypeID = jobTypeID;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+  public String toString() {
+        return "Job{" +
+                "jobID='" + jobID + '\'' +
+                ", jobTypeID='" + jobTypeID + '\'' +
+                ", startTime=" + startTime +
+                ", duration=" + duration +
+                '}';
+    }
 }
