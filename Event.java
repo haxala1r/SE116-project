fc53fdfd1226b620ef425e14136fae1c35146b1d
Emin Arslan <arslanemin2662@gmail.com>
2024-05-16 21:45:52 +0300

    Fleshed out Event

--- Event.java
+++ Event.java
@@ -1,3 +1,15 @@
 public class Event {
-    
+	private String msg;
+	private double time;
+	public Event(String msg, double time) {
+		this.msg = msg;
+		this.time = time;
+	}
+
+	public double getTime() {
+		return time;
+	}
+	public String getMsg() {
+		return msg;
+	}
 }
